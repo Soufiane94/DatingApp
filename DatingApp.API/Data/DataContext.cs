bd47b45c0a94f6d940eab180662657cfb4d83788
Soufiane94 <soufiane94.iha@gmail.com>
2019-04-03 20:23:30 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        //constructor
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        
+        //Dbset property, its conventional to pluralise the name of the entities
+
+       
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
